3f934f2d77067eb51ed6a8de62e913b945c025f8
xuhuanle <xuhuanle@xuhuanle-OptiPlex-780.(none)>
2013-08-29 14:05:19 +0800

    new

---
+++ branding.js
@@ -0,0 +1,7 @@
+function ucbheader(){
+	document.write('<img src="CUHK.jpg" border="0" usemap="#UCBMap" /><map name="UCBMap" id="UCBMap"><area shape="rect" coords="11,3,298,27" href="http://www.cuhk.edu.hk/" alt="The Chinese University of Hong Kong" target="_blank" /><area shape="rect" coords="794,7,828,25" href="http://www.colorado.edu/" alt="CU Home" target="_blank" /><area shape="rect" coords="835,7,873,25" href="http://www.colorado.edu/search/" alt="CU Search" target="_blank"  /><area shape="rect" coords="880,7,916,25" href="http://www.colorado.edu/atoz/" alt="CU A to Z" target="_blank"  /><area shape="rect" coords="925,7,949,25" href="http://www.colorado.edu/campusmap/" alt="Campus Map" target="_blank" /></map>');
+}
+
+function ucbfooter(){
+	document.write('<a target="_blank" href="http://www.ie.cuhk.edu.hk"><img src="wordmark.png" alt="The department of Information Engineering" /></a>');	
+}
